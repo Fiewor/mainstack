19074796a3e351bcffc07fb2e09a281cec2e63d9
Fiewor <johnfiewor@gmail.com>
2024-04-11 21:18:51 +0100

    chore: add mui + its dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import "./styles/index.scss";
 
 const queryClient = new QueryClient();
